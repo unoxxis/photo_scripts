265c050486b7f0220255333e655a258b3b427948
Unoxxis <82263780+unoxxis@users.noreply.github.com>
2022-08-07 09:14:59 +0200

    Zwischenversion

--- pixum_prepare.zsh
+++ pixum_prepare.zsh
@@ -7,7 +7,8 @@ usage() {
 	echo "usage: $0 [OPTIONS] [-o out-folder] in-folder [in-folder ...]"
     echo
     echo "OPTIONS:"
-    echo " -o | --output           Specify output folder instead of working in place."
+    echo " -o | --output path      Specify output folder instead of working in place."
+    echo " -t | --tempdir path     Specify temp dir, only needed if -o is used."
     echo " -v | --verbose          Increase verbosity (up to two times)."
     echo " --help                  Show this help."
 }
@@ -39,6 +40,7 @@ command -v exiv2 >/dev/null 2>&1 || {
 verbose=0
 separateout=0
 outpath="<none>"
+temppath="${TMPDIR:-/tmp}"
 pathlist=()
 
 if [[ $# -lt 1 ]]; then
@@ -52,12 +54,14 @@ while [ "$1" != "" ]; do
             shift
             separateout=1
             outpath="$1"
-            echo "ERROR: -o option is not implemented yet."
-            exit 99
             ;;
         --help)
             usage
             exit 0
+            ;;
+        -t | --tempdir)
+            shift
+            temppath="$1"
             ;;
         -v | --verbose )
             ((verbose+=1))
@@ -72,9 +76,13 @@ while [ "$1" != "" ]; do
     shift
 done
 
-exiv_verb=""
-if [[ $verbose -gt 0 ]]; then
-    exiv_verb="-v"
+exiv_verb=("-Q" "w")
+mvflag=
+if [[ $verbose -gt 1 ]]; then
+    exiv_verb=("-Q" "d" "-v")
+    mvflag=-v
+elif [[ $verbose -gt 0 ]]; then
+    exiv_verb=("-Q" "i" "-v")
 fi
 
 if [[ ${#pathlist} -lt 1 ]]; then
@@ -87,6 +95,7 @@ echovv "Parsing done, results:"
 echovv "OPTIONS:"
 echovv " Output separate   $separateout"
 echovv " Output path       $outpath"
+echovv " Temp path         $temppath"
 echovv
 echovv "PATH LIST:"
 echovv "${pathlist[@]}"
@@ -94,19 +103,45 @@ echovv
 
 for currpath in "${pathlist[@]}"; do
 	echo "Processing $currpath..."
+    prefix_default="${currpath##*/}"
+
+    if [[ $separateout -gt 0 ]]; then
+        # Create Tempdir if separate output is required,
+        # so that the original files are not modified!
+        echovv "Creating temporary working directory..."
+        workdir=$(mktemp -d --tmpdir="$temppath")
+        echov "Working directory is $workdir"
+        echov "Copying files to temporary directory..."
+        cd "$currpath"
+        find . -type f -iregex '.*\.\(jpeg\|jpg\|JPEG\|JPG\|png\|tiff\)' -execdir cp $mvflag --parents -t $workdir {} +
+        # Change currpath to work on working directory from now on
+        currpath="$workdir"
+    fi
 
 	echov "Changing mdate to match exif date..."
-    find "$currpath" -type f -iregex '.*\.\(jpeg\|jpg\|JPEG\|JPG\|png\|tiff\)' -exec exiv2 $exiv_verb -T {} \;
+    find "$currpath" -type f -iregex '.*\.\(jpeg\|jpg\|JPEG\|JPG\|png\|tiff\)' -execdir exiv2 "${exiv_verb[@]}" -T {} +
 
     echov "Sorting files..."
     sortfiles=()
     readarray -t sortfiles < <(find "$currpath" -type f -iregex '.*\.\(jpeg\|jpg\|JPEG\|JPG\|png\|tiff\)' -printf "%T@\t%p\n" | sort -n | cut -f 2)
 
-    prefix="${currpath##*/}"
+    # Ask prefix
+    read -p "Enter prefix for files [$prefix_default]: " prefix
+    prefix=${prefix:-$prefix_default}
+
+    # Rename Files
     currnum=1
     for file in "${sortfiles[@]}"; do
-        fname=$(printf "%s/%s %04d.%s" $(dirname "$file") "$prefix" $currnum "${file##*.}")
-        mv -v "$file" "$fname"
+        if [[ $separateout -eq 0 ]]; then
+            # If unset, target directory is set per file in case subdirs exist.
+            outpath=$(dirname "$file")
+        fi
+        fname=$(printf "%s/%s_%04d.%s" "$outpath" "$prefix" $currnum "${file##*.}")
+        mv $mvflag "$file" "$fname"
         currnum=$((currnum+1)) 
     done
+
+    if [[ $separateout -gt 0 ]]; then
+        rm -rf $mvflag "$workdir"
+    fi
 done
